ab40e33f80cb331809655f0955b45018d313dbbf
idmanalo <122817029+idmanalo@users.noreply.github.com>
2024-02-17 13:59:17 -0500

    Create TestFile.py

---
+++ TestFile.py
@@ -0,0 +1,9 @@
+name = "adA LOveLace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ida"
+last_name = "manalo"
+full_name = f"{first_name} {last_name}"
+print(f"\tWazzup \n\t{full_name.title()}!")
